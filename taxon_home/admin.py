90b8ea26135bf4336119f5445f4542abebf73d85
andyoberlin <andyoberlin@gmail.com>
2013-01-18 14:36:58 -0500

    Got rid of dynamic nav bar and useless tables in schema again 2

--- taxon_home/admin.py
+++ taxon_home/admin.py
@@ -1,11 +1,7 @@
 from taxon_home.models import *
 from django.contrib import admin
 
-admin.site.register(NavBarOption)
-admin.site.register(DropDownItem)
 admin.site.register(Picture)
-admin.site.register(PictureType)
-admin.site.register(PictureProp)
 admin.site.register(TagGroup)
 admin.site.register(Tag)
 admin.site.register(TagColor)
