0c3428411fd0f6e16fbb17e0ce3459f198555732
andyoberlin <andyoberlin@gmail.com>
2013-01-18 14:36:05 -0500

    Got rid of dynamic nav bar and useless tables in schema again

--- taxon_home/views/pagelets/public/NavBarPagelet.py
+++ taxon_home/views/pagelets/public/NavBarPagelet.py
@@ -5,7 +5,6 @@
 	Date: July 23, 2012
 '''
 from renderEngine.PageletBase import PageletBase
-from taxon_home.models import NavBarOption, PictureType, PictureProp
 
 class NavBarPagelet(PageletBase):
 	def __init__(self, addHelpButton=False):
@@ -25,11 +24,8 @@ class NavBarPagelet(PageletBase):
 		
 		if (request.method == "GET" and request.GET.has_key('add_login')):                
 			is_admin_page = request.GET['add_login']
-		
-		optionsList = NavBarOption.objects.all().order_by('rank')
 
 		return {
-			'optionsList' : optionsList,
 			'is_admin_page' : is_admin_page,
 			'addHelpButton' : self.addHelpButton
 		}
