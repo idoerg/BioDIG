76adcdfca4bf45d33e5a513b7b8cd2b7543dd9b3
Andrew Oberlin <andyoberlin@gmail.com>
2014-01-15 18:33:32 -0500

    Added the post for tagGroups

--- biodig/base/exceptions.py
+++ biodig/base/exceptions.py
@@ -1,8 +1,22 @@
 from rest_framework.exceptions import APIException
 
-'''
-    Bad Request Exception.
-'''
 class BadRequestException(APIException):
+    '''
+        Bad Request Exception.
+    '''
     status_code = 400
     detail = "A Bad Request was made for the API. Revise input parameters."
+    
+class ImageDoesNotExist(APIException):
+    '''
+        Image does not exist error.
+    '''
+    status_code = 404
+    detail = "The image key provided does not refer to an image in this database."
+    
+class DatabaseIntegrity(APIException):
+    '''
+        An error occurred while trying to write to the database.
+    '''
+    status_code = 500
+    detail = 'An error occurred while trying to write to the database.'
--- biodig/rest/v2/TagGroups/forms.py
+++ biodig/rest/v2/TagGroups/forms.py
@@ -7,9 +7,12 @@
     @author: Andrew Oberlin
 '''
 from django import forms
-from biodig.base.models import TagGroup
+from biodig.base.models import TagGroup, Picture
 from biodig.base import forms as bioforms
 from biodig.base.serializers import TagGroupSerializer
+from biodig.base.exceptions import ImageDoesNotExist, DatabaseIntegrity
+from rest_framework.exceptions import PermissionDenied
+from django.db import transaction, DatabaseError
 
 class MultiGetForm(forms.Form):
     # Query Parameters
@@ -62,21 +65,31 @@ class MultiGetForm(forms.Form):
         return TagGroupSerializer(query, many=True).data
 
 class PostForm(forms.Form):
+    image = forms.IntegerField(required=True)
+    name = forms.CharField(required=True)
 
-    def clean(self):
-        '''
-            Cleans the data for this form to normalize parameters.
-        '''
-        return self.cleaned_data
-
-    def submit(self):
+    def submit(self, request):
         '''
             Submits the form for creating a TagGroup
             once the form has cleaned the input data.
         '''
-        group = None
+        try:
+            image = Picture.objects.get(pk__exact=self.cleaned_data['image'])
+        except (Picture.DoesNotExist, ValueError):
+            raise ImageDoesNotExist()
+        
+        if not image.writePermissions(request.user):
+            raise PermissionDenied()
+        
+        # start saving the new tag now that it has passed all tests
+        tagGroup = TagGroup(name=self.cleaned_data['name'], picture=image, user=request.user)
+        try:
+            tagGroup.save()
+        except DatabaseError:
+            transaction.rollback()
+            raise DatabaseIntegrity()
 
-        return TagGroupSerializer(group).data
+        return TagGroupSerializer(tagGroup).data
 
 class DeleteForm(forms.Form):
 
