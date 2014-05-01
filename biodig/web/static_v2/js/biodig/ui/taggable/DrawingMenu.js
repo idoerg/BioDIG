cd789e327ce80afdb3226679930c71503eb315f9
Andrew Oberlin <andyoberlin@gmail.com>
2014-05-01 14:35:13 -0400

    Fancy color picker

--- biodig/web/static_v2/js/biodig/ui/taggable/DrawingMenu.js
+++ biodig/web/static_v2/js/biodig/ui/taggable/DrawingMenu.js
@@ -34,7 +34,7 @@ define(deps, function($, _, settings, DrawingMenuTmpl) {
         	},
         	onChange: function (hsb, hex, rgb) {
         		$colorPicker.css('background-color', '#' + hex);
-                console.log(rgb);
+                self.color = rgb;
         	}
         });
 
