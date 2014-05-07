7480d7e8d89a18e5a26aa272b67f2393d4e5b4f9
Andrew Oberlin <andyoberlin@gmail.com>
2014-05-07 19:01:17 -0400

    Oops.

--- biodig/web/static_v2/js/biodig/storage/ImageDao.js
+++ biodig/web/static_v2/js/biodig/storage/ImageDao.js
@@ -179,7 +179,7 @@ define(deps, function($, ImageClient, ImageOrganismClient, TagGroupClient, TagCl
 
         if (this.tagGroups_cache == null) {
             return $.Deferred(function() {
-                this.tagGroups()
+                self.tagGroups()
                     .done(function() {
                         $.when(add())
                             .done(function(group) {
