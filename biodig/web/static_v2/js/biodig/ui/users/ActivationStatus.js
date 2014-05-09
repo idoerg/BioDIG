5179e9ba06ed5c10a868cc11418799a0a1f68d4c
Andrew Oberlin <andyoberlin@gmail.com>
2014-05-09 16:53:38 -0400

    Fixed jquery problem with template.

--- biodig/web/static_v2/js/biodig/ui/users/ActivationStatus.js
+++ biodig/web/static_v2/js/biodig/ui/users/ActivationStatus.js
@@ -7,7 +7,7 @@ define(deps, function($, _, settings, UserClient, ActivationStatusTmpl) {
 
     function ActivationStatus(selector) {
         this.$container = $(selector);
-        this.$el = ActivationStatusTemplate({ 'status' : 'BEGIN', 'settings' : settings });
+        this.$el = $(ActivationStatusTemplate({ 'status' : 'BEGIN', 'settings' : settings }));
         this.$container.append(this.$el);
         this.client = UserClient.create();
     }
@@ -17,20 +17,20 @@ define(deps, function($, _, settings, UserClient, ActivationStatusTmpl) {
         this.client.update(user_id, { 'activation_key' : activation_key })
             .done(function(user) {
                 self.$container.remove(self.$el);
-                self.$el = ActivationStatusTemplate({
+                self.$el = $(ActivationStatusTemplate({
                     'status' : 'SUCCESS',
                     'user' : user,
                     'settings' : settings
-                });
+                }));
                 self.$container.append(self.$el);
             })
             .fail(function(e) {
                 self.$container.remove(self.$el);
-                self.$el = ActivationStatusTemplate({
+                self.$el = $(ActivationStatusTemplate({
                     'status' : 'ERROR',
                     'detail' : e.detail || e.message,
                     'settings' : settings
-                });
+                }));
                 self.$container.append(self.$el);
             });
     };
